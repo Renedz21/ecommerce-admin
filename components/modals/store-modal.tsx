e9682258fb039375fdad2bf2509b1a1bb20e2913
Renedz21 <edzonperez.castillo@gmail.com>
2023-12-26 19:05:00 -0500

    Create a form to create stores in prisma

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -3,9 +3,32 @@
 import Modal from "@/components/common/modal"
 import { useStoreModal } from "@/hooks/use-store-modal"
 
+import * as z from 'zod'
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+    name: z.string().min(1, { message: "Name is required." }),
+})
+
 export const StoreModal = () => {
 
     const storeModal = useStoreModal()
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+        },
+    })
+
+    const onSubmit = (data: z.infer<typeof formSchema>) => {
+        console.log(data)
+    }
+
     return (
         <Modal
             title="Create Store"
@@ -13,7 +36,42 @@ export const StoreModal = () => {
             isOpen={storeModal.isOpen}
             onClose={storeModal.onClose}
         >
-            Future Create Store Form
+            <div className="">
+                <div className="space-y-4 pb-4">
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)}>
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>
+                                            Name
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Input type="text" placeholder="Enter your store name" {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <div className="flex items-center justify-end w-full pt-6 space-x-2">
+                                <Button
+                                    variant='destructive'
+                                    onClick={storeModal.onClose}
+                                >
+                                    Cancel
+                                </Button>
+                                <Button
+                                    type="submit"
+                                >
+                                    Create Store
+                                </Button>
+                            </div>
+                        </form>
+                    </Form>
+                </div>
+            </div>
         </Modal>
     )
 }
